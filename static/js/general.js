da3291db80f371c620f91a6e6c9b0e4388768a89
subher0 <subher0@mail.ru>
2017-08-07 23:01:59 +0300

    Smart navbar

--- static/js/general.js
+++ static/js/general.js
@@ -1,3 +1,25 @@
 $('#modal-content').on('shown.bs.modal', function() {
        $("body.modal-open").removeAttr("style");
  });
+
+let lastWindowPosition = window.pageYOffset;
+let navbar = null;
+let navtop= 0;
+
+window.onload = () => {
+    navbar = document.querySelector('.navbar');
+};
+
+window.addEventListener('scroll', (event) => {
+    let coordDifference = lastWindowPosition - window.pageYOffset;
+    if (coordDifference < 0 && navbar.offsetHeight > -navtop) {
+        navtop+= coordDifference;
+    } else if (coordDifference > 0 && navtop < 0) {
+        navtop+= coordDifference
+    }
+    if (navtop > 0) {
+        navtop = 0;
+    }
+    navbar.setAttribute('style', `top: ${navtop}`);
+    lastWindowPosition = window.pageYOffset;
+});
