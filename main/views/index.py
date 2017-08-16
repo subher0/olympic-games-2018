295678f30b1327e7258465dfdbaed4efe4fdb3c2
subher0 <subher0@mail.ru>
2017-08-16 20:33:48 +0300

    News on index page

--- main/views/index.py
+++ main/views/index.py
@@ -9,13 +9,13 @@ from main.models import News
 def index_view(request):
     template = loader.get_template('main/index.html')
     projects = Project.objects.getSomeProjects(4)
-    news = News.objects.getSomeNews(1)
+    news = News.objects.getSomeNews(3)
     tiles = IndexTile.objects.getAllTiles()
     topTiles = tiles[:2]
     restTiles = tiles[2:]
     context = {
         'projects': projects,
-        'news': news[0],
+        'news': news,
         'topTiles': topTiles,
         'tiles': restTiles,
     }
--- static/js/index.js
+++ static/js/index.js
@@ -6,3 +6,34 @@ $('.project-slider').slick({
     autoplaySpeed: 2000,
     arrows: false
 });
+
+window.onload(() => {
+    let ring4 = document.querySelector('.ring-4');
+    let ring5 = document.querySelector('.ring-5');
+    let id4 = 0;
+    let id5 = 0;
+
+    ring4.addEventListener('mouseout', () => {
+        clearTimeout(id4);
+        id4 = setTimeout(() => {
+            ring4.setAttribute('style', 'z-index: 0')
+        }, 200);
+    });
+
+    ring4.addEventListener('mouseenter', () => {
+        clearTimeout(id4);
+        ring4.setAttribute('style', 'z-index: 1')
+    });
+
+    ring5.addEventListener('mouseout', () => {
+        clearTimeout(id5);
+        id5 = setTimeout(() => {
+            ring5.setAttribute('style', 'z-index: 0')
+        }, 200);
+    });
+
+    ring5.addEventListener('mouseenter', () => {
+        clearTimeout(id5);
+        ring5.setAttribute('style', 'z-index: 1');
+    });
+});
