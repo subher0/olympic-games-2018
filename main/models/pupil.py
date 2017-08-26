72747d31d182894407c2604dcb05f5c967eb0124
subher0 <subher0@mail.ru>
2017-08-26 16:36:22 +0300

    Fixed model

--- main/models/pupil.py
+++ main/models/pupil.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Pupil(models.Model):
-    name = models.CharField(verbose_name='Name', )
-    surname = models.CharField(verbose_name='Surname')
+    name = models.CharField(verbose_name='Name', max_length=255)
+    surname = models.CharField(verbose_name='Surname', max_length=255)
     email = models.EmailField(verbose_name='Email')
-    school = models.CharField(verbose_name='School')
-    grade = models.CharField(verbose_name='Grade')
+    school = models.CharField(verbose_name='School', max_length=255)
+    grade = models.ImageField(verbose_name='Grade')
