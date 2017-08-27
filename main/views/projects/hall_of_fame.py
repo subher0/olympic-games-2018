c3bc2457a6f94e3134e8370953f38a15dda66dfe
subher0 <subher0@mail.ru>
2017-08-27 15:40:31 +0300

    Added stub for famous ones template

--- main/views/projects/hall_of_fame.py
+++ main/views/projects/hall_of_fame.py
@@ -2,11 +2,14 @@ from django.http import HttpResponse, Http404
 from django.shortcuts import get_object_or_404
 from django.template import loader, TemplateDoesNotExist
 
-from main.models import Chump
+from main.models import Chump, FamousOne
 
 
 def hall_of_fame_view(request):
-    context = {}
+    famousOnes = FamousOne.objects.getAllFamousOnes()
+    context = {
+        'famousOnes': famousOnes
+    }
     try:
         template = loader.get_template('main/projects/hall_of_fame.html')
     except TemplateDoesNotExist:
