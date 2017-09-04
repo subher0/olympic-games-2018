79c928cae71e8abb7a8e19fa898747949159a3ec
subher0 <subher0@mail.ru>
2017-09-04 22:25:28 +0300

    Fixed js error with reg button

--- static/projects/university_saturdays/saturdays_script.js
+++ static/projects/university_saturdays/saturdays_script.js
@@ -1,4 +1,12 @@
 window.addEventListener('load', () => {
+    let reg_buttons = document.querySelectorAll('.reg-button');
+    let reg_window = document.querySelector('.registration-form-wrapper');
+    let close_button = document.querySelector('.close-button');
+    close_button.addEventListener('click', (event) => {
+        event.preventDefault();
+        reg_window.setAttribute('class', 'registration-form-wrapper hidden');
+    });
+
     reg_buttons.forEach((item) => {
         item.addEventListener('click', (event) => {
             reg_window.setAttribute('class', 'registration-form-wrapper');
