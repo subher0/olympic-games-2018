197583c9767f89a4de1ef51a7140421bf3f3e3b6
subher0 <subher0@mail.ru>
2017-09-15 16:07:41 +0300

    Changed regexp pattern

--- main/forms/event_reg_form.py
+++ main/forms/event_reg_form.py
@@ -4,7 +4,7 @@ from django import forms
 class EventSignupForm(forms.Form):
     nameField = forms.CharField(label='name', max_length=30)
     emailField = forms.EmailField(label='email', max_length=30)
-    phoneField = forms.RegexField(regex=r'^(\+7|8)[\(-]?\d{3}[-\)]?\d{3}-?\d{2}-?\d{2}$', label='phone', max_length=30)
+    phoneField = forms.RegexField(regex=r'^(\+7|8)?[\(\s-]?\d{3}[\)\s-]?\d{3}[-\s]?\d{2}[-\s]?\d{2}$', label='phone', max_length=30)
     schoolField = forms.CharField(label='school', max_length=60)
     gradeField = forms.IntegerField(label='grade')
     eventId = forms.IntegerField(label='eventId')
