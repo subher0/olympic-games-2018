b8344ee1019acda116a39315929376312c221018
nagmed <rnagmed@mail.ru>
2017-09-28 23:04:10 +0300

    Changed debug status

--- olympic_games/settings.py
+++ olympic_games/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*!%la6ogq_=krau1ukfr3s9b4nm4454jb%+zmbw$d$88o*j&41'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['37.204.2.4', 'olimp05.ru', 'www.olimp05.ru', 'dev.olimp05.ru', '127.0.0.1', '25.80.248.164']
 
